291ef14eb5ff5b8b2fec01d30eb1905456eaef42
Diontre Pate <Diontre.Pate@uline.com>
2019-05-28 08:13:27 -0500

    I have added a gps tracker that will display lat, long linesand also an animation to start mapping that to coordinates

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
-@Component({
+//these are my instance variable for my gps.js file
+
+declare var getLocation: any;
+declare var myMove: any;
+
+@Component
+({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'Shuttle';
+
+  f()
+  {
+    new getLocation();
+  }
+  move()
+  {
+    new myMove();
+  }
+
+
 }
---
+++ client/src/assets/gps.js
@@ -0,0 +1,51 @@
+var myObj, myJSON;
+
+function getLocation()
+   {
+  if (navigator.geolocation)
+   {
+
+    navigator.geolocation.watchPosition(showPosition);
+
+    }
+    else
+    {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position)
+{
+  //the code below is just for debugging purposes
+  //also the variable for coordinates updates when the coordinate change
+
+
+ //var y = position.coords.latitude;
+  //console.log(y);
+ //console.log("ldjflsj");
+  document.getElementById("demo").innerHTML="Latitude: " + position.coords.latitude +
+                           "<br>Longitude: " + position.coords.longitude;
+
+}
+
+function myMove()
+ {
+  var elem = document.getElementById("myAnimation");
+  var pos = 0;
+  var id = setInterval(frame, 10);
+  function frame()
+  {
+    if (pos == 350)
+    {
+      clearInterval(id);
+    }
+    else
+    {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+
+
