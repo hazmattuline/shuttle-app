0c543c6680eb15e8c98a585668a15255a2766226
Emily Steinebrey <Emily.Steinebrey@uline.com>
2019-05-30 15:28:59 -0500

    lat/lon points in right place - next task - different browser sizes and have points move right way when browser size changes

--- client/src/assets/gps.js
+++ client/src/assets/gps.js
@@ -46,38 +46,56 @@ function myMove()
   var elem8 = document.getElementById("animate8");
   var elem9 = document.getElementById("animate9");
   var elem10 = document.getElementById("animate10");
+  var elem11 = document.getElementById("animate11");
+  var elem12 = document.getElementById("animate12");
+  var elem13 = document.getElementById("animate13");
+  var elem14 = document.getElementById("animate14");
+  var elem15 = document.getElementById("animate15");
+  var elem16 = document.getElementById("animate16");
+  var elem17 = document.getElementById("animate17");
+  var elem18 = document.getElementById("animate18");
+  var elem19 = document.getElementById("animate19");
+  var elem20 = document.getElementById("animate20");
+  var elem21 = document.getElementById("animate21");
+  var elem22 = document.getElementById("animate22");
+
   /*elem.style.top = '20px';
   elem.style.left = '300px'; */
 
-  var lat1 = 42.523278; // biggest latitude in image
-  var lon1 = -87.971868; // smallest longitude in image
-  var lat3 = 42.513481; // smallest latitude in image
-  var lon3 = -87.952126;  // biggest longitude in image
+  // var lat1 = 42.523221; // biggest latitude in image
+  // var lon1 = -87.971868; // smallest longitude in image
+  // var lat3 = 42.513481; // smallest latitude in image
+  // var lon3 = -87.952126;  // biggest longitude in image
+
+  var lat1 = 42.523300; // biggest latitude in image
+  var lon1 = -87.971642; // smallest longitude in image
+  var lat3 = 42.5130865; // smallest latitude in image
+  var lon3 = -87.951814;  // biggest longitude in image
 
   var lats = [];
   var lons = [];
 
- //H1 pond 42.521774,-87.95953
- var lat2 = 42.521774;
- var lon2 =  -87.95953; 
+ //H1 left 42.522131, -87.960058
+ var lat2 = 42.522131;
+ var lon2 = -87.960058; 
  lats.push(lat2);
  lons.push(lon2);
 
-  //Under H1 
-  var lat2 = 42.520177;
-  var lon2 = -87.957892;
+  //H1 pickup - 42.521301, -87.959559 
+  var lat2 = 42.521301;
+  var lon2 =  -87.959559;
   lats.push(lat2);
   lons.push(lon2);
 
-  // Front of H2 42.514972, -87.954458
- var lat2 = 42.514972;
- var lon2 =  -87.954458;
+  // H1 right - 42.522068, -87.959082
+ var lat2 = 42.522068;
+ var lon2 = -87.959082;
  lats.push(lat2);
  lons.push(lon2);
 
-  // bend of road - 42.520050, -87.952599
- var lat2 = 42.520050;
- var lon2 =  -87.952599;
+  // H1 entering parking lot - 42.522143, -87.956657
+ var lat2 = 42.522143;
+ var lon2 =  -87.956657;
  lats.push(lat2);
  lons.push(lon2);
  
@@ -85,15 +103,15 @@ function myMove()
 //var lat2 = 42.520041;
 //var lon2 = -87.956545;
 
- // leftmost warehouse
- var lat2 = 42.521171;
- var lon2 = -87.969865;
+ // near H1 on route - 42.520969, -87.954506
+ var lat2 = 42.520969;
+ var lon2 =  -87.954506;
  lats.push(lat2);
  lons.push(lon2);
 
- // rightmost warehouse 42.519214,-87.964078
- var lat2 = 42.519214;
- var lon2 = -87.964078;
+ // H2 left of door - 42.515357, -87.953827
+ var lat2 = 42.515357;
+ var lon2 = -87.953827;
  lats.push(lat2);
  lons.push(lon2);
 
@@ -102,15 +120,15 @@ function myMove()
  //var lon2 = -87.959579;
  
  
- // middle of grass - 42.516505, -87.961380 
- var lat2 = 42.516505;
- var lon2 = -87.961380;
+ // H2 - middle right - 42.514566, -87.953792
+ var lat2 = 42.514566;
+ var lon2 = -87.953792;
  lats.push(lat2);
  lons.push(lon2);
 
- // road between warehouses - 42.522424, -87.966594
- var lat2 = 42.522424;
- var lon2 = -87.966594;
+ // H2 touching highway - 42.515021, -87.952295
+ var lat2 = 42.515021;
+ var lon2 = -87.952295;
  lats.push(lat2);
  lons.push(lon2);
 
@@ -118,17 +136,92 @@ function myMove()
  //var lat2 = 42.516592;
  //var lon2 = -87.962571;
 
- // middle bottom right warehouse - 42.516307, -87.964041
- var lat2 = 42.516307;
- var lon2 =  -87.964041;
+ // H2 right before door - 42.515088, -87.954471
+ var lat2 = 42.515088;
+ var lon2 = -87.954471;
  lats.push(lat2);
  lons.push(lon2);
 
- // Bristol Pottery - 42.513734, -87.969400
- var lat2 = 42.513734;
- var lon2 =  -87.969400;
+ // On highway middle near H2 - 42.518583, -87.952389
+ var lat2 = 42.518583;
+ var lon2 =  -87.952389;
  lats.push(lat2);
  lons.push(lon2);
+
+ // 11 On highway just leaving H2 - 42.515481,-87.952282
+ var lat2 = 42.515481;
+ var lon2 =  -87.952282;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ // 12 On highway H2 - in front of left lot - 42.515887, -87.952303
+ var lat2 = 42.515887;
+ var lon2 =  -87.952303;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ //13 H2_highwaybyedgeoflot - 42.516456, -87.952357
+ var lat2 = 42.516456;
+ var lon2 =  -87.952357;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ // 14 highway_H1_towardstoppartofloop - 42.521580, -87.956123
+ var lat2 = 42.521580;
+ var lon2 =  -87.956123;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ // 15 H1_near centerofloop - 42.521359, -87.955715
+ var lat2 = 42.521359;
+ var lon2 =  -87.955715;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ // 16 H1_nearingcurveinroad_afterloop - 42.520734, -87.953746
+ var lat2 = 42.520734;
+ var lon2 =  -87.953746;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ // 17 H1 closer to curve of road - 42.520335, -87.952985
+ var lat2 = 42.520335;
+ var lon2 = -87.952985;
+ lats.push(lat2);
+ lons.push(lon2); 
+
+ // 18 H2 - highway by parking lot - 42.516120, -87.952277
+ var lat2 = 42.516120;
+ var lon2 =  -87.952277;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ // 19 H1 near curve in road - 42.519931, -87.952559
+ var lat2 = 42.519931;
+ var lon2 =  -87.952559;
+ lats.push(lat2);
+ lons.push(lon2);
+
+
+ // 20 H1 right after highway curve - 42.519504, -87.952420
+ var lat2 = 42.519504;
+ var lon2 =  -87.952420;
+ lats.push(lat2);
+ lons.push(lon2);
+
+ // 21 r1_middle_highwayforest - 42.517788, -87.952388
+ var lat2 = 42.517788;
+ var lon2 =  -87.952388;
+ lats.push(lat2);
+ lons.push(lon2);
+
+
+ // 22 H2_highwayafterlot - 42.516997, -87.952399
+ var lat2 = 42.516997;
+ var lon2 = -87.952399;
+ lats.push(lat2);
+ lons.push(lon2);
+
 
 
   var height = 600;
@@ -167,16 +260,48 @@ function myMove()
 
   var posxs = [];
   var posys = [];
+  var H2boundary = 42.516;
+  var hwyboundary1 = 42.5175;
+  var hwyboundary2 = 42.519;
+  var H1boundary = 42.52;
 
   for (i = 0; i < lats.length; i++) {
-    var posx = (londists[i])/(maxlondist)*width - 25*(latdists[i]/maxlatdist);//60*Math.pow(londist/maxlondist, 1.5);
-  
-    if (lats[i] > (lat1+lat3)/2) {
-      var posy = (latdists[i])/(maxlatdist)*height + 125;
+
+    if (lats[i] < H2boundary) {
+      var posx = (londists[i])/(maxlondist)*width;
     }
-    else {
-      var posy = (latdists[i])/(maxlatdist)*height + 100;
+    else if ((lats[i] >= H2boundary) && (lats[i] <= hwyboundary1)) {
+      var posx = (londists[i])/(maxlondist)*width - 10;
     }
+    else if (lats[i] > hwyboundary1 && lats[i] < hwyboundary2) {
+      var posx = (londists[i])/(maxlondist)*width - 19;
+    }
+    else /*(lats[i] > hwyboundary1 && lats[i] < H1boundary)*/ {
+      var posx = (londists[i])/(maxlondist)*width - 30;
+    }
+    // else {
+    //   console.log("heyyy");
+    //   var posx = (londists[i])/(maxlondist)*width - 25;
+    // }
+
+
+    //60*Math.pow(londist/maxlondist, 1.5); - 25*(latdists[i]/maxlatdist)
+    //if (lats[i] < H2boundary) {
+      if (lats[i] > H1boundary) {
+        var posy = (latdists[i])/(maxlatdist)*height + 135;
+      }
+      else {
+        var posy = (latdists[i])/(maxlatdist)*height + 125;
+      }
+      
+    //}
+    
+    // if (lats[i] > (lat1+lat3)/2) {
+    //   var posy = (latdists[i])/(maxlatdist)*height + 125;
+    // }
+    // else {
+    //   var posy = (latdists[i])/(maxlatdist)*height + 100;
+    // }
     posxs.push(posx);
     posys.push(posy);
   }
@@ -213,8 +338,8 @@ function myMove()
 
   // console.log("posx="+posx)
   // console.log("posy="+posy);
-  console.log("posxs="+posxs);
-  console.log("posys="+posys);
+  // console.log("posxs="+posxs);
+  // console.log("posys="+posys);
 
   elem1.style.top = posys[0] + 'px';
   elem1.style.left = posxs[0] + 'px';
@@ -236,6 +361,30 @@ function myMove()
   elem9.style.left = posxs[8] + 'px';
   elem10.style.top = posys[9] + 'px';
   elem10.style.left = posxs[9] + 'px';
+  elem11.style.top = posys[10] + 'px';
+  elem11.style.left = posxs[10] + 'px';
+  elem12.style.top = posys[11] + 'px';
+  elem12.style.left = posxs[11] + 'px';
+  elem13.style.top = posys[12] + 'px';
+  elem13.style.left = posxs[12] + 'px';
+  elem14.style.top = posys[13] + 'px';
+  elem14.style.left = posxs[13] + 'px';
+  elem15.style.top = posys[14] + 'px';
+  elem15.style.left = posxs[14] + 'px';
+  elem16.style.top = posys[15] + 'px';
+  elem16.style.left = posxs[15] + 'px';
+  elem17.style.top = posys[16] + 'px';
+  elem17.style.left = posxs[16] + 'px';
+  elem18.style.top = posys[17] + 'px';
+  elem18.style.left = posxs[17] + 'px';
+  elem19.style.top = posys[18] + 'px';
+  elem19.style.left = posxs[18] + 'px';
+  elem20.style.top = posys[19] + 'px';
+  elem20.style.left = posxs[19] + 'px';
+  elem21.style.top = posys[20] + 'px';
+  elem21.style.left = posxs[20] + 'px';
+  elem22.style.top = posys[21] + 'px';
+  elem22.style.left = posxs[21] + 'px';
 
 
  // var lat = 42.521774;
