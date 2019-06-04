7bf1b690b4f135f59b7261ed4a70340e4ab2e759
Diontre Pate <Diontre.Pate@uline.com>
2019-06-04 08:04:48 -0500

    its lit

--- client/src/app/driver/driver.component.ts
+++ client/src/app/driver/driver.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+
+declare var toggle: any;
+
+@Component
+({
   selector: 'app-driver',
   templateUrl: './driver.component.html',
   styleUrls: ['./driver.component.css']
 })
-export class DriverComponent implements OnInit {
-
+export class DriverComponent implements OnInit
+ {
+  
+  tog()
+  {
+    new toggle();
+  }
   constructor() { }
 
   ngOnInit() {
