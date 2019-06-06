b9c65ca5899db83f3fed5c15e083825732f0ea28
Emily Steinebrey <Emily.Steinebrey@uline.com>
2019-06-06 10:46:00 -0500

    creating new feature branch

--- server/src/main/java/com/uline/shuttle/app/configuration/WebMvcConfiguration.java
+++ server/src/main/java/com/uline/shuttle/app/configuration/WebMvcConfiguration.java
@@ -1,7 +1,6 @@
 package com.uline.shuttle.app.configuration;
 
 import java.io.IOException;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -20,14 +19,21 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/ui/**/*", "/ui/", "/ui").addResourceLocations("classpath:/static/ui/")
-        .setCachePeriod(0).resourceChain(true).addResolver(new PathResourceResolver() {
-          @Override
-          protected Resource getResource(String resourcePath, Resource location) throws IOException {
-            Resource requestedResource = location.createRelative(resourcePath);
-            return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                : new ClassPathResource("/static/ui/index.html");
-          }
-        });
+    registry
+        .addResourceHandler("/ui/**/*", "/ui/", "/ui")
+        .addResourceLocations("classpath:/static/ui/")
+        .setCachePeriod(0)
+        .resourceChain(true)
+        .addResolver(
+            new PathResourceResolver() {
+              @Override
+              protected Resource getResource(String resourcePath, Resource location)
+                  throws IOException {
+                Resource requestedResource = location.createRelative(resourcePath);
+                return requestedResource.exists() && requestedResource.isReadable()
+                    ? requestedResource
+                    : new ClassPathResource("/static/ui/index.html");
+              }
+            });
   }
 }
