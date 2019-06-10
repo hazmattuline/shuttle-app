54b03a00ca5f320086ed1aa12f39da2daf56044f
Diontre Pate <Diontre.Pate@uline.com>
2019-06-10 17:43:31 -0500

    PR

--- shuttle-client/src/app/driver/driver.component.ts
+++ shuttle-client/src/app/driver/driver.component.ts
@@ -1,93 +1,111 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
 import { ScriptService } from '../script.service';
-import { errorHandler } from '@angular/platform-browser/src/browser';
 
 
 
-@Component 
-({ 
-selector: 'app-driver', 
-templateUrl: './driver.component.html', 
-styleUrls: ['./driver.component.css'] 
-}) 
-export class DriverComponent implements OnInit 
-{ 
+@Component
+  ({
+    selector: 'app-driver',
+    templateUrl: './driver.component.html',
+    styleUrls: ['./driver.component.css']
+  })
+export class DriverComponent implements OnInit {
 
-constructor(private supportService: ScriptService) {}
-
-
-ngOnInit() { }
-
-options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-
-count: number = 0; 
-watchID; 
+  constructor(private supportService: ScriptService) { }
 
 
-errorHandler( err ) {
-   
-  if (err.code == 1) {
-     
-     // access is denied
+  ngOnInit() { }
+
+  options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  count: number = 0;
+  watchID;
+
+
+  errorHandler(err) {
+
+    if (err.code == 1) {
+
+      // access is denied
+    }
   }
-}
 
-toggle1(){
-this.count++;
-console.log(this.count);
-if (this.count%2 == 0){document.getElementById("demo").innerHTML="OFF";}
-else {document.getElementById("demo").innerHTML="ON";}}
-
-
-newtog(){ 
-this.count++;
-if (this.count%2 == 0){alert("YOU ARE NOW INACTIVE"); 
-document.getElementById("demo2").innerHTML="Inactive";}
-else{alert("YOU ARE NOW ACTIVE"); 
-document.getElementById("demo2").innerHTML="Active";}}
+  toggle1() {
+    this.count++;
+    console.log(this.count);
+    if (this.count % 2 == 0) { document.getElementById("demo").innerHTML = "OFF"; }
+    else { document.getElementById("demo").innerHTML = "ON"; }
+  }
 
 
-newcell(){ var r = confirm("ARE YOU SURE!"); 
-if (r == true){} // find row to copy 
-/* send something to database*/ 
-else { /*send nothing*/}} 
+  newtog() {
+    this.count++;
+    if (this.count % 2 == 0) {
+      alert("YOU ARE NOW INACTIVE");
+      document.getElementById("demo2").innerHTML = "Inactive";
+    }
+    else {
+      alert("YOU ARE NOW ACTIVE");
+      document.getElementById("demo2").innerHTML = "Active";
+    }
+  }
 
 
-promptMe(){ 
-if (this.count%2 == 0){
-var endMi = prompt("What is the ending Mileage pleaase?");
-var fuelAm = prompt("Home much Fuel went in today?"); 
-var fuelCos = prompt("What was the cost of the fuel?"); }
-else{
-var vehicleResp = prompt("Please Enter The Vehicle you will be using today"); 
-var beginMi = prompt("What is the starting mileage please?"); }}
-
-inactivate(){if (this.count%2 == 0){
-navigator.geolocation.clearWatch(this.watchID); 
-console.log("disabled tracking")}
-else{this.getLocation();}} 
+  newcell() {
+    var r = confirm("ARE YOU SURE!");
+    if (r == true) { } // find row to copy 
+    /* send something to database*/
+    else { /*send nothing*/ }
+  }
 
 
-getLocation(){if (navigator.geolocation){ 
-this.watchID = navigator.geolocation.watchPosition(this.showPosition,errorHandler,this.options);} 
-else{document.getElementById("demo").innerHTML= "Geolocation is not supported by this browser.";}} 
-showPosition(position){document.getElementById("demo3").innerHTML="Latitude: " + position.coords.latitude + 
-"<br>Longitude: " + position.coords.longitude; 
-var x = position.coords.latitude; 
-var y = position.coords.longitude; 
-console.log(x); 
-console.log(y);}
+  promptMe() {
+    if (this.count % 2 == 0) {
+      var endMi = prompt("What is the ending Mileage pleaase?");
+      var fuelAm = prompt("Home much Fuel went in today?");
+      var fuelCos = prompt("What was the cost of the fuel?");
+    }
+    else {
+      var vehicleResp = prompt("Please Enter The Vehicle you will be using today");
+      var beginMi = prompt("What is the starting mileage please?");
+    }
+  }
 
-fuel(){
-var fuelAm = prompt("Home much Fuel went in?");
-var fuelCos = prompt("What was the cost of the fuel?");}
+  inactivate() {
+    if (this.count % 2 == 0) {
+      navigator.geolocation.clearWatch(this.watchID);
+      console.log("disabled tracking")
+    }
+    else { this.getLocation(); }
+  }
 
-getwords()
-{
- //send this response somewhere
-}
+
+  getLocation() {
+    if (navigator.geolocation) {
+      this.watchID = navigator.geolocation.watchPosition(this.showPosition, this.errorHandler, this.options);
+    }
+    else { document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser."; }
+  }
+
+  showPosition(position) {
+    document.getElementById("demo3").innerHTML = "Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude;
+    var x = position.coords.latitude;
+    var y = position.coords.longitude;
+    console.log(x);
+    console.log(y);
+  }
+
+  fuel() {
+    var fuelAm = prompt("Home much Fuel went in?");
+    var fuelCos = prompt("What was the cost of the fuel?");
+  }
+
+  getwords() {
+    //send this response somewhere
+  }
 }
--- shuttle-client/src/app/user/user.component.ts
+++ shuttle-client/src/app/user/user.component.ts
@@ -1,103 +1,128 @@
-import { Component } from '@angular/core';
-import { ScriptService} from '../script.service';
+import { Component, OnInit } from '@angular/core';
+import { ScriptService } from '../script.service';
 //these are my instance variable for my gps.js file
 
-declare var getLocation: any;
-declare var myMove: any;
-
 @Component
-({
-  selector: 'app-driver',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
-})
-export class UserComponent {
+  ({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
+  })
+
+export class UserComponent implements OnInit {
   title = 'Shuttle';
+  watchID;
+  x;
+  y;
+  options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
 
-  f()
-  {
-    new getLocation();
-  }
-  move()
-  {
-    new myMove();
-  }
-
-  ngOnInit() 
-  {
-  
+  ngOnInit() {
   }
 
   constructor(private scriptService: ScriptService) { }
 
-  myMove(){
-  var elem1 = document.getElementById("animate1");
-  var lat1 = 42.523300; // biggest latitude in image
-  var lon1 = -87.971642; // smallest longitude in image
-  var lat3 = 42.5130865; // smallest latitude in image
-  var lon3 = -87.951814;  // biggest longitude in image
-  var lats = [];
-  var lons = [];
-  //need to retrieve this from database later 
-  var lat2 = 42.521873;
-  var lon2 = -87.959583; 
-  lats.push(lat2);
-  lons.push(lon2);
 
-  var height = 600;
-  var width = 900;
-  var londists = [];
-  var latdists = [];
-  
-  for (var i = 0; i < lats.length; i++) {
-    var londist = (lons[i]-lon1)*Math.cos(Math.abs(lats[i]));
-    // latitude is pretty constant
-    var latdist = (lat1-lats[i]);
-    latdists.push(latdist);
-    londists.push(londist);}
+  errorHandler(err) {
 
-   var maxlatdist = (lat1-lat3); 
-   var maxlondist = (lon3-lon1)*(Math.cos(lat1)+Math.cos(lat3))/2; // (lat1+lat3)/2
+    if (err.code == 1) {
 
-   var posxs = [];
-   var posys = [];
-   var H2boundary = 42.516;
-   var H2door = 42.514;
-   var hwyboundary1 = 42.5175;
-   var hwyboundary2 = 42.519;
-   var H1boundary = 42.52;
-   var h2lonbound = -87.953;
-
-   for (i = 0; i < lats.length; i++) {
-    // x coordinate
-   if (lats[i] < H2boundary && lons[i] < h2lonbound) {
-   var posx = (londists[i])/(maxlondist)*width + 3; 
+      // access is denied
     }
-   else if ((lats[i] < H2boundary) && (lons[i] > h2lonbound)) {
-   var posx = (londists[i])/(maxlondist)*width + 3; 
-    }
-   else if ((lats[i] >= H2boundary) && (lats[i] <= hwyboundary1)) {
-   var posx = (londists[i])/(maxlondist)*width - 6; 
-    }
-   else if (lats[i] > hwyboundary1 && lats[i] < hwyboundary2) {
-   var posx = (londists[i])/(maxlondist)*width - 15; 
-    }
-   else if (lats[i] > hwyboundary2 && lons[i] > h2lonbound) {
-   var posx = (londists[i])/(maxlondist)*width - 30; 
-    } 
-   else  {
-   var posx = (londists[i])/(maxlondist)*width - 27;
-    }
-   if (lats[i] > H1boundary) {
-   var posy = (latdists[i])/(maxlatdist)*height;
-    }
-   else {
-   var posy = (latdists[i])/(maxlatdist)*height - 6;
-    }
-    posxs.push(posx);
-    posys.push(posy);
   }
-   elem1.style.top = posys[0] + 'px';
-   elem1.style.left = posxs[0] + 'px';
-}
+
+
+  getLocation() {
+    if (navigator.geolocation) {
+      this.watchID = navigator.geolocation.watchPosition(this.showPosition.bind(this), this.errorHandler, this.options);
+    }
+    else { document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser."; }
+  }
+
+  showPosition(position) {
+    document.getElementById("demo3").innerHTML = "Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude;
+     this.x = position.coords.latitude;
+     this.y = position.coords.longitude;
+    console.log(this.x);
+    console.log(this.y);
+    this.myMove();
+  }
+  
+  myMove() {
+
+    console.log('my move');
+    var elem1 = document.getElementById("animate1");
+    var lat1 = 42.523300; // biggest latitude in image
+    var lon1 = -87.971642; // smallest longitude in image
+    var lat3 = 42.5130865; // smallest latitude in image
+    var lon3 = -87.951814;  // biggest longitude in image
+    var lats = [];
+    var lons = [];
+    //need to retrieve this from database later 
+    let lat2 = this.x;
+    let lon2 = this.y;
+    lats.push(lat2);
+    lons.push(lon2);
+
+    var height = 600;
+    var width = 900;
+    var londists = [];
+    var latdists = [];
+
+    for (var i = 0; i < lats.length; i++) {
+      var londist = (lons[i] - lon1) * Math.cos(Math.abs(lats[i]));
+      // latitude is pretty constant
+      var latdist = (lat1 - lats[i]);
+      latdists.push(latdist);
+      londists.push(londist);
+    }
+
+    var maxlatdist = (lat1 - lat3);
+    var maxlondist = (lon3 - lon1) * (Math.cos(lat1) + Math.cos(lat3)) / 2; // (lat1+lat3)/2
+
+    var posxs = [];
+    var posys = [];
+    var H2boundary = 42.516;
+    var H2door = 42.514;
+    var hwyboundary1 = 42.5175;
+    var hwyboundary2 = 42.519;
+    var H1boundary = 42.52;
+    var h2lonbound = -87.953;
+
+    for (i = 0; i < lats.length; i++) {
+      // x coordinate
+      if (lats[i] < H2boundary && lons[i] < h2lonbound) {
+        var posx = (londists[i]) / (maxlondist) * width + 3;
+      }
+      else if ((lats[i] < H2boundary) && (lons[i] > h2lonbound)) {
+        var posx = (londists[i]) / (maxlondist) * width + 3;
+      }
+      else if ((lats[i] >= H2boundary) && (lats[i] <= hwyboundary1)) {
+        var posx = (londists[i]) / (maxlondist) * width - 6;
+      }
+      else if (lats[i] > hwyboundary1 && lats[i] < hwyboundary2) {
+        var posx = (londists[i]) / (maxlondist) * width - 15;
+      }
+      else if (lats[i] > hwyboundary2 && lons[i] > h2lonbound) {
+        var posx = (londists[i]) / (maxlondist) * width - 30;
+      }
+      else {
+        var posx = (londists[i]) / (maxlondist) * width - 27;
+      }
+      if (lats[i] > H1boundary) {
+        var posy = (latdists[i]) / (maxlatdist) * height;
+      }
+      else {
+        var posy = (latdists[i]) / (maxlatdist) * height - 6;
+      }
+      posxs.push(posx);
+      posys.push(posy);
+    }
+    elem1.style.top = posys[0] + 'px';
+    elem1.style.left = posxs[0] + 'px';
+  }
 }
