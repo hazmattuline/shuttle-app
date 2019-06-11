2caca85cbb902afdae286b74957a1743008d1422
Diontre Pate <Diontre.Pate@uline.com>
2019-06-11 12:57:06 -0500

    everything changed with TSlint

--- shuttle-client/src/app/driver/driver.component.ts
+++ shuttle-client/src/app/driver/driver.component.ts
@@ -11,24 +11,27 @@ import { ScriptService } from '../script.service';
   })
 export class DriverComponent implements OnInit {
 
-  constructor(private supportService: ScriptService) { }
-
-
-  ngOnInit() { }
-
   options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
   };
 
-  count: number = 0;
+  count = 0;
   watchID;
+  x;
+  y;
+
+  constructor(private supportService: ScriptService) { }
+
+
+  ngOnInit() { }
+
 
 
   errorHandler(err) {
 
-    if (err.code == 1) {
+    if (err.code === 1) {
 
       // access is denied
     }
@@ -37,75 +40,70 @@ export class DriverComponent implements OnInit {
   toggle1() {
     this.count++;
     console.log(this.count);
-    if (this.count % 2 == 0) { document.getElementById("demo").innerHTML = "OFF"; }
-    else { document.getElementById("demo").innerHTML = "ON"; }
+    if (this.count % 2 === 0) { document.getElementById('demo').innerHTML = 'OFF'; 
+  } else { document.getElementById('demo').innerHTML = 'ON'; }
   }
 
 
   newtog() {
     this.count++;
-    if (this.count % 2 == 0) {
-      alert("YOU ARE NOW INACTIVE");
-      document.getElementById("demo2").innerHTML = "Inactive";
-    }
-    else {
-      alert("YOU ARE NOW ACTIVE");
-      document.getElementById("demo2").innerHTML = "Active";
+    if (this.count % 2 === 0) {
+      alert('YOU ARE NOW INACTIVE');
+      document.getElementById('demo2').innerHTML = 'Inactive';
+    } else {
+      alert('YOU ARE NOW ACTIVE');
+      document.getElementById('demo2').innerHTML = 'Active';
     }
   }
 
 
   newcell() {
-    var r = confirm("ARE YOU SURE!");
-    if (r == true) { } // find row to copy 
+    const r = confirm('ARE YOU SURE!');
+    if (r === true) {
+    }  else { /*send nothing*/ }// find row to copy 
     /* send something to database*/
-    else { /*send nothing*/ }
   }
 
-
   promptMe() {
-    if (this.count % 2 == 0) {
-      var endMi = prompt("What is the ending Mileage pleaase?");
-      var fuelAm = prompt("Home much Fuel went in today?");
-      var fuelCos = prompt("What was the cost of the fuel?");
-    }
-    else {
-      var vehicleResp = prompt("Please Enter The Vehicle you will be using today");
-      var beginMi = prompt("What is the starting mileage please?");
+    if (this.count % 2 === 0) {
+      const endMi = prompt('What is the ending Mileage pleaase?');
+      const fuelAm = prompt('Home much Fuel went in today?');
+      const fuelCos = prompt('What was the cost of the fuel?');
+    } else {
+      const vehicleResp = prompt('Please Enter The Vehicle you will be using today');
+      const beginMi = prompt('What is the starting mileage please?');
     }
   }
 
   inactivate() {
-    if (this.count % 2 == 0) {
+    if (this.count % 2 === 0) {
       navigator.geolocation.clearWatch(this.watchID);
-      console.log("disabled tracking")
-    }
-    else { this.getLocation(); }
+      console.log('disabled tracking');
+    } else { this.getLocation(); }
   }
 
 
   getLocation() {
     if (navigator.geolocation) {
       this.watchID = navigator.geolocation.watchPosition(this.showPosition, this.errorHandler, this.options);
-    }
-    else { document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser."; }
+    } else { document.getElementById('demo').innerHTML = 'Geolocation is not supported by this browser.'; }
   }
 
   showPosition(position) {
-    document.getElementById("demo3").innerHTML = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
-    var x = position.coords.latitude;
-    var y = position.coords.longitude;
-    console.log(x);
-    console.log(y);
+    document.getElementById('demo3').innerHTML = 'Latitude: ' + position.coords.latitude +
+      '<br>Longitude: ' + position.coords.longitude;
+    this.x = position.coords.latitude;
+    this.y = position.coords.longitude;
+    console.log(this.x);
+    console.log(this.y);
   }
 
   fuel() {
-    var fuelAm = prompt("Home much Fuel went in?");
-    var fuelCos = prompt("What was the cost of the fuel?");
+    const fuelAm = prompt('Home much Fuel went in?');
+    const fuelCos = prompt('What was the cost of the fuel?');
   }
 
   getwords() {
-    //send this response somewhere
+    // send this response somewhere
   }
 }
--- shuttle-client/src/app/user/user.component.ts
+++ shuttle-client/src/app/user/user.component.ts
@@ -14,6 +14,10 @@ export class UserComponent implements OnInit {
   watchID;
   x;
   y;
+  posx;
+  posy;
+  posxs = [];
+  posys = [];
   options = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -28,7 +32,7 @@ export class UserComponent implements OnInit {
 
   errorHandler(err) {
 
-    if (err.code == 1) {
+    if (err.code === 1) {
 
       // access is denied
     }
@@ -38,91 +42,82 @@ export class UserComponent implements OnInit {
   getLocation() {
     if (navigator.geolocation) {
       this.watchID = navigator.geolocation.watchPosition(this.showPosition.bind(this), this.errorHandler, this.options);
-    }
-    else { document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser."; }
+    } else { document.getElementById('demo').innerHTML = 'Geolocation is not supported by this browser.'; }
   }
 
   showPosition(position) {
-    document.getElementById("demo3").innerHTML = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
-     this.x = position.coords.latitude;
-     this.y = position.coords.longitude;
+    document.getElementById('demo3').innerHTML = 'Latitude:' + position.coords.latitude +
+      '<br>Longitude: ' + position.coords.longitude;
+    this.x = position.coords.latitude;
+    this.y = position.coords.longitude;
     console.log(this.x);
     console.log(this.y);
     this.myMove();
   }
-  
   myMove() {
 
     console.log('my move');
-    var elem1 = document.getElementById("animate1");
-    var lat1 = 42.523300; // biggest latitude in image
-    var lon1 = -87.971642; // smallest longitude in image
-    var lat3 = 42.5130865; // smallest latitude in image
-    var lon3 = -87.951814;  // biggest longitude in image
-    var lats = [];
-    var lons = [];
-    //need to retrieve this from database later 
-    let lat2 = this.x;
-    let lon2 = this.y;
+    const elem1 = document.getElementById('animate1');
+    const lat1 = 42.523300; // biggest latitude in image
+    const lon1 = -87.971642; // smallest longitude in image
+    const lat3 = 42.5130865; // smallest latitude in image
+    const lon3 = -87.951814;  // biggest longitude in image
+    const lats = [];
+    const lons = [];
+    // need to retrieve this from database later 
+
+    const lat2 = this.x;
+    const lon2 = this.y;
     lats.push(lat2);
     lons.push(lon2);
 
-    var height = 600;
-    var width = 900;
-    var londists = [];
-    var latdists = [];
+    const height = 600;
+    const width = 900;
+    const londists = [];
+    const latdists = [];
 
-    for (var i = 0; i < lats.length; i++) {
-      var londist = (lons[i] - lon1) * Math.cos(Math.abs(lats[i]));
+    for (let i = 0; i < lats.length; i++) {
+      const londist = (lons[i] - lon1) * Math.cos(Math.abs(lats[i]));
       // latitude is pretty constant
-      var latdist = (lat1 - lats[i]);
+      const latdist = (lat1 - lats[i]);
       latdists.push(latdist);
       londists.push(londist);
     }
 
-    var maxlatdist = (lat1 - lat3);
-    var maxlondist = (lon3 - lon1) * (Math.cos(lat1) + Math.cos(lat3)) / 2; // (lat1+lat3)/2
+    const maxlatdist = (lat1 - lat3);
+    const maxlondist = (lon3 - lon1) * (Math.cos(lat1) + Math.cos(lat3)) / 2; // (lat1+lat3)/2
 
-    var posxs = [];
-    var posys = [];
-    var H2boundary = 42.516;
-    var H2door = 42.514;
-    var hwyboundary1 = 42.5175;
-    var hwyboundary2 = 42.519;
-    var H1boundary = 42.52;
-    var h2lonbound = -87.953;
+    const H2boundary = 42.516;
+    const H2door = 42.514;
+    const hwyboundary1 = 42.5175;
+    const hwyboundary2 = 42.519;
+    const H1boundary = 42.52;
+    const h2lonbound = -87.953;
 
-    for (i = 0; i < lats.length; i++) {
+    for (let i = 0; i < lats.length; i++) {
       // x coordinate
       if (lats[i] < H2boundary && lons[i] < h2lonbound) {
-        var posx = (londists[i]) / (maxlondist) * width + 3;
-      }
-      else if ((lats[i] < H2boundary) && (lons[i] > h2lonbound)) {
-        var posx = (londists[i]) / (maxlondist) * width + 3;
-      }
-      else if ((lats[i] >= H2boundary) && (lats[i] <= hwyboundary1)) {
-        var posx = (londists[i]) / (maxlondist) * width - 6;
-      }
-      else if (lats[i] > hwyboundary1 && lats[i] < hwyboundary2) {
-        var posx = (londists[i]) / (maxlondist) * width - 15;
-      }
-      else if (lats[i] > hwyboundary2 && lons[i] > h2lonbound) {
-        var posx = (londists[i]) / (maxlondist) * width - 30;
-      }
-      else {
-        var posx = (londists[i]) / (maxlondist) * width - 27;
+        this.posx = (londists[i]) / (maxlondist) * width + 3;
+      } else if ((lats[i] < H2boundary) && (lons[i] > h2lonbound)) {
+        this.posx = (londists[i]) / (maxlondist) * width + 3;
+      } else if ((lats[i] >= H2boundary) && (lats[i] <= hwyboundary1)) {
+        this.posx = (londists[i]) / (maxlondist) * width - 6;
+      } else if (lats[i] > hwyboundary1 && lats[i] < hwyboundary2) {
+        this.posx = (londists[i]) / (maxlondist) * width - 15;
+      } else if (lats[i] > hwyboundary2 && lons[i] > h2lonbound) {
+        this.posx = (londists[i]) / (maxlondist) * width - 30;
+      } else {
+        this.posx = (londists[i]) / (maxlondist) * width - 27;
       }
       if (lats[i] > H1boundary) {
-        var posy = (latdists[i]) / (maxlatdist) * height;
+        this.posy = (latdists[i]) / (maxlatdist) * height;
+      } else {
+        this.posy = (latdists[i]) / (maxlatdist) * height - 6;
       }
-      else {
-        var posy = (latdists[i]) / (maxlatdist) * height - 6;
-      }
-      posxs.push(posx);
-      posys.push(posy);
+      this.posxs.push(this.posx);
+      this.posys.push(this.posy);
     }
-    elem1.style.top = posys[0] + 'px';
-    elem1.style.left = posxs[0] + 'px';
+    elem1.style.top = this.posys[0] + 'px';
+    elem1.style.left = this.posxs[0] + 'px';
   }
 }
