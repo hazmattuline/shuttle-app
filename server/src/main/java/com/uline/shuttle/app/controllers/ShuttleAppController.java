f5c0e0a90e1ec5c970c19c789d844b7fd1436189
Diontre Pate <Diontre.Pate@uline.com>
2019-06-20 16:33:50 -0500

    resolved the typo

--- server/src/main/java/com/uline/shuttle/app/controllers/ShuttleAppController.java
+++ server/src/main/java/com/uline/shuttle/app/controllers/ShuttleAppController.java
@@ -1,8 +1,5 @@
 package com.uline.shuttle.app.controllers;
 
-import com.uline.common.metrics.ExecutionTime;
-import com.uline.shuttle.app.services.ShuttleAppService;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,6 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.uline.common.metrics.ExecutionTime;
+import com.uline.shuttle.app.services.ShuttleAppService;
+
+import io.swagger.annotations.ApiOperation;
 import rest.models.requests.CoordinateRequest;
 import rest.models.requests.ShiftRequest;
 import rest.models.response.CoordinateResponse;
@@ -28,14 +30,13 @@ public class ShuttleAppController {
   }
 
   @ExecutionTime("ShuttleAppService.enRoute")
-  @ApiOperation(
-      value = "posting the coordinates that we get from driver view and storing in a database")
+  @ApiOperation(value = "posting the coordinates that we get from driver view and storing in a database")
   @PatchMapping(value = "/enRoute")
   public CoordinateResponse enRoute(@RequestBody CoordinateRequest coordinateRequest) {
     return shuttleAppService.enRoute(coordinateRequest);
   }
 
-  @ApiOperation("value getting the coordinates from the database")
+  @ApiOperation("fetching coordinates from the database")
   @GetMapping(value = "/receiveCoords/{vehicleName}")
   public CoordinateResponse receiveCoordinates(@PathVariable("vehicleName") String vehicleName) {
     return this.shuttleAppService.getCoordinates(vehicleName);
