0ce6090a6912d20da56dde3c585cb0a722bcb632
Emily Steinebrey <Emily.Steinebrey@uline.com>
2019-06-24 13:10:17 -0500

    cleaning assets folder

--- shuttle-client/src/assets/requestscript.js
+++
@@ -1,3 +0,0 @@
-function submitRequest() {
-    alert("Your request has been submitted.\n WARNING: You still must get to the door in time. \n The driver will wait 1-2 minutes for you when they arrive.");
-}
