8f80a6d7588bf4b56b536f12164ef112ea5ae7fb
Emily.Steinebrey <Emily.Steinebrey@L54964-V54964.ulinedm.com>
2019-06-25 14:26:59 -0500

    Resolved merge issue

--- server/src/main/java/com/uline/shuttle/app/services/ShuttleAppService.java
+++ server/src/main/java/com/uline/shuttle/app/services/ShuttleAppService.java
@@ -1,11 +1,9 @@
 package com.uline.shuttle.app.services;
 
 import rest.models.requests.CoordinateRequest;
-import rest.models.requests.FuelRequest;
 import rest.models.requests.PassengerRequest;
 import rest.models.requests.StartRequest;
 import rest.models.response.CoordinateResponse;
-import rest.models.response.FuelResponse;
 import rest.models.response.PassengerResponse;
 import rest.models.response.StartResponse;
 import rest.models.response.VehicleOptionsResponse;
@@ -13,8 +11,6 @@ import rest.models.response.VehicleOptionsResponse;
 public interface ShuttleAppService {
 
 	CoordinateResponse enRoute(CoordinateRequest coordinateRequest);
-
-	FuelResponse fuelResponse(FuelRequest fuelRequest);
 
 	CoordinateResponse getCoordinates(Integer vehicleID);
 
