192b565691b6e1363bd0227dbcee608dcf50d60f
Emily.Steinebrey <Emily.Steinebrey@L54964-V54964.ulinedm.com>
2019-06-26 13:26:35 -0500

    Resolving merge issues

--- server/src/main/java/com/uline/shuttle/app/client/impl/ShuttleAppClientImpl.java
+++ server/src/main/java/com/uline/shuttle/app/client/impl/ShuttleAppClientImpl.java
@@ -1,8 +1,9 @@
 package com.uline.shuttle.app.client.impl;
 
+import com.uline.ha.rest.UlineRestTemplate;
+import com.uline.shuttle.app.client.ShuttleAppClient;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,10 +11,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import com.uline.ha.rest.UlineRestTemplate;
-import com.uline.shuttle.app.client.ShuttleAppClient;
-
 import rest.models.requests.CoordinateRequest;
 import rest.models.requests.FuelRequest;
 import rest.models.requests.PassengerRequest;
@@ -27,121 +24,121 @@ import rest.models.response.VehicleOptionsResponse;
 @Service
 public class ShuttleAppClientImpl implements ShuttleAppClient {
 
-	@Value("${shuttle.service.base.url}")
-	private String baseUrl;
+  @Value("${shuttle.service.base.url}")
+  private String baseUrl;
 
-	private UlineRestTemplate restTemplate;
+  private UlineRestTemplate restTemplate;
 
-	@Value("${shuttle.service.rc.url.for.vehicle.options}")
-	private String shuttleServiceForVehicleOptions;
+  @Value("${shuttle.service.rc.url.for.vehicle.options}")
+  private String shuttleServiceForVehicleOptions;
 
-	@Value("${shuttle.service.rc.url.post.passenger.data}")
-	private String shuttleServiceForPassenger;
+  @Value("${shuttle.service.rc.url.post.passenger.data}")
+  private String shuttleServiceForPassenger;
 
-	@Value("${shuttle.service.rc.url.get.coordinates}")
-	private String shuttleServiceForGet;
+  @Value("${shuttle.service.rc.url.get.coordinates}")
+  private String shuttleServiceForGet;
 
-	@Value("${shuttle.service.rc.url.post.startOfShift}")
-	private String shuttleServiceStartOfShift;
+  @Value("${shuttle.service.rc.url.post.startOfShift}")
+  private String shuttleServiceStartOfShift;
 
-	@Value("${shuttle.service.rc.url.post.coordinates}")
-	private String shuttleServiceUrl;
+  @Value("${shuttle.service.rc.url.post.coordinates}")
+  private String shuttleServiceUrl;
 
-	@Value("${shuttle.service.rc.url.post.fuel}")
-	private String shuttleServiceForFuel;
+  @Value("${shuttle.service.rc.url.post.fuel}")
+  private String shuttleServiceForFuel;
 
-	@Autowired
-	public ShuttleAppClientImpl(UlineRestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
+  @Autowired
+  public ShuttleAppClientImpl(UlineRestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
 
-	@Override
-	public CoordinateResponse enRoute(CoordinateRequest coordinateRequest) {
+  @Override
+  public CoordinateResponse enRoute(CoordinateRequest coordinateRequest) {
 
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceUrl);
+    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceUrl);
 
-		return restTemplate
-				.exchange(
-						builder.build().toUriString(),
-						HttpMethod.PATCH,
-						new HttpEntity<>(coordinateRequest),
-						new ParameterizedTypeReference<CoordinateResponse>() {})
-				.getBody();
-	}
+    return restTemplate
+        .exchange(
+            builder.build().toUriString(),
+            HttpMethod.PATCH,
+            new HttpEntity<>(coordinateRequest),
+            new ParameterizedTypeReference<CoordinateResponse>() {})
+        .getBody();
+  }
 
-	@Override
-	public CoordinateResponse getCoordinates(Integer vehicleID) {
+  @Override
+  public CoordinateResponse getCoordinates(Integer vehicleID) {
 
-		Map<String, Integer> params = new HashMap<>();
-		params.put("vehicleID", vehicleID);
+    Map<String, Integer> params = new HashMap<>();
+    params.put("vehicleID", vehicleID);
 
-		UriComponentsBuilder builder =
-				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForGet);
+    UriComponentsBuilder builder =
+        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForGet);
 
-		return restTemplate
-				.exchange(
-						builder.buildAndExpand(params).toUriString(),
-						HttpMethod.GET,
-						new HttpEntity<>(null, null),
-						new ParameterizedTypeReference<CoordinateResponse>() {})
-				.getBody();
-	}
+    return restTemplate
+        .exchange(
+            builder.buildAndExpand(params).toUriString(),
+            HttpMethod.GET,
+            new HttpEntity<>(null, null),
+            new ParameterizedTypeReference<CoordinateResponse>() {})
+        .getBody();
+  }
 
-	@Override
-	public VehicleOptionsResponse getVehicleOptions() {
-		UriComponentsBuilder builder =
-				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForVehicleOptions);
+  @Override
+  public VehicleOptionsResponse getVehicleOptions() {
+    UriComponentsBuilder builder =
+        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForVehicleOptions);
 
-		return restTemplate
-				.exchange(
-						builder.build().toUriString(),
-						HttpMethod.GET,
-						null,
-						new ParameterizedTypeReference<VehicleOptionsResponse>() {})
-				.getBody();
-	}
+    return restTemplate
+        .exchange(
+            builder.build().toUriString(),
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<VehicleOptionsResponse>() {})
+        .getBody();
+  }
 
-	@Override
-	public StartResponse startShift(StartRequest startRequest) {
+  @Override
+  public StartResponse startShift(StartRequest startRequest) {
 
-		UriComponentsBuilder builder =
-				UriComponentsBuilder.fromUriString(baseUrl + ShuttleServiceStartOfShift);
+    UriComponentsBuilder builder =
+        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceStartOfShift);
 
-		return restTemplate
-				.exchange(
-						builder.build().toUriString(),
-						HttpMethod.POST,
-						new HttpEntity<>(startRequest),
-						new ParameterizedTypeReference<StartResponse>() {})
-				.getBody();
-	}
+    return restTemplate
+        .exchange(
+            builder.build().toUriString(),
+            HttpMethod.POST,
+            new HttpEntity<>(startRequest),
+            new ParameterizedTypeReference<StartResponse>() {})
+        .getBody();
+  }
 
-	@Override
-	public FuelResponse storeFuel(FuelRequest fuelRequest) {
-		UriComponentsBuilder builder =
-				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForFuel);
+  @Override
+  public FuelResponse storeFuel(FuelRequest fuelRequest) {
+    UriComponentsBuilder builder =
+        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForFuel);
 
-		return restTemplate
-				.exchange(
-						builder.build().toUriString(),
-						HttpMethod.POST,
-						new HttpEntity<>(fuelRequest),
-						new ParameterizedTypeReference<FuelResponse>() {})
-				.getBody();
-	}
+    return restTemplate
+        .exchange(
+            builder.build().toUriString(),
+            HttpMethod.POST,
+            new HttpEntity<>(fuelRequest),
+            new ParameterizedTypeReference<FuelResponse>() {})
+        .getBody();
+  }
 
-	@Override
-	public PassengerResponse storePassengers(PassengerRequest passengerRequest) {
+  @Override
+  public PassengerResponse storePassengers(PassengerRequest passengerRequest) {
 
-		UriComponentsBuilder builder =
-				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForPassenger);
+    UriComponentsBuilder builder =
+        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForPassenger);
 
-		return restTemplate
-				.exchange(
-						builder.build().toUriString(),
-						HttpMethod.POST,
-						new HttpEntity<>(passengerRequest),
-						new ParameterizedTypeReference<PassengerResponse>() {})
-				.getBody();
-	}
+    return restTemplate
+        .exchange(
+            builder.build().toUriString(),
+            HttpMethod.POST,
+            new HttpEntity<>(passengerRequest),
+            new ParameterizedTypeReference<PassengerResponse>() {})
+        .getBody();
+  }
 }
