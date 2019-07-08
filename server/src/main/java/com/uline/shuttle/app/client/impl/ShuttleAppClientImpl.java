c7acc13342f751d6f4e56b9842d46065d0c66de4
Emily.Steinebrey <Emily.Steinebrey@L54964-V54964.ulinedm.com>
2019-07-08 10:50:29 -0500

    Testing endpoints

--- server/src/main/java/com/uline/shuttle/app/client/impl/ShuttleAppClientImpl.java
+++ server/src/main/java/com/uline/shuttle/app/client/impl/ShuttleAppClientImpl.java
@@ -1,10 +1,9 @@
 package com.uline.shuttle.app.client.impl;
 
-import com.uline.ha.rest.UlineRestTemplate;
-import com.uline.shuttle.app.client.ShuttleAppClient;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -12,6 +11,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.uline.ha.rest.UlineRestTemplate;
+import com.uline.shuttle.app.client.ShuttleAppClient;
+
 import rest.models.requests.CoordinateRequest;
 import rest.models.requests.FuelRequest;
 import rest.models.requests.PassengerRequest;
@@ -26,177 +29,176 @@ import rest.models.response.VehicleOptionsResponse;
 @Service
 public class ShuttleAppClientImpl implements ShuttleAppClient {
 
-  @Value("${shuttle.service.base.url}")
-  private String baseUrl;
+	@Value("${shuttle.service.base.url}")
+	private String baseUrl;
 
-  private UlineRestTemplate restTemplate;
+	private UlineRestTemplate restTemplate;
 
-  @Value("${shuttle.service.rc.url.for.vehicle.options}")
-  private String shuttleServiceForVehicleOptions;
+	@Value("${shuttle.service.rc.url.for.vehicle.options}")
+	private String shuttleServiceForVehicleOptions;
 
-  @Value("${shuttle.service.rc.url.post.passenger.data}")
-  private String shuttleServiceForPassenger;
+	@Value("${shuttle.service.rc.url.post.passenger.data}")
+	private String shuttleServiceForPassenger;
 
-  @Value("${shuttle.service.rc.url.get.coordinates}")
-  private String shuttleServiceForGet;
+	@Value("${shuttle.service.rc.url.get.coordinates}")
+	private String shuttleServiceForGet;
 
-  @Value("${shuttle.service.rc.url.post.startOfShift}")
-  private String shuttleServiceStartOfShift;
+	@Value("${shuttle.service.rc.url.post.startOfShift}")
+	private String shuttleServiceStartOfShift;
 
-  @Value("${shuttle.service.rc.url.post.coordinates}")
-  private String shuttleServiceUrl;
+	@Value("${shuttle.service.rc.url.post.coordinates}")
+	private String shuttleServiceUrl;
 
-  @Value("${shuttle.service.rc.url.post.fuel}")
-  private String shuttleServiceForFuel;
+	@Value("${shuttle.service.rc.url.post.fuel}")
+	private String shuttleServiceForFuel;
 
-  @Value("shuttle.service.rc.url.get.active.shuttles")
-  private String activeShuttlesURL;
+	@Value("${shuttle.service.rc.url.get.active.shuttles}")
+	private String activeShuttlesURL;
 
-  @Value("shuttle.service.rc.url.mark.active")
-  private String markActiveURL;
+	@Value("${shuttle.service.rc.url.mark.active}")
+	private String markActiveURL;
 
-  @Value("shuttle.service.rc.url.mark.inactive")
-  private String markInactiveURL;
+	@Value("${shuttle.service.rc.url.mark.inactive}")
+	private String markInactiveURL;
 
-  @Autowired
-  public ShuttleAppClientImpl(UlineRestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
+	@Autowired
+	public ShuttleAppClientImpl(UlineRestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
-  @Override
-  public CoordinateResponse enRoute(CoordinateRequest coordinateRequest) {
+	@Override
+	public CoordinateResponse enRoute(CoordinateRequest coordinateRequest) {
 
-    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceUrl);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceUrl);
 
-    return restTemplate
-        .exchange(
-            builder.build().toUriString(),
-            HttpMethod.PATCH,
-            new HttpEntity<>(coordinateRequest),
-            new ParameterizedTypeReference<CoordinateResponse>() {})
-        .getBody();
-  }
+		return restTemplate
+				.exchange(
+						builder.build().toUriString(),
+						HttpMethod.PATCH,
+						new HttpEntity<>(coordinateRequest),
+						new ParameterizedTypeReference<CoordinateResponse>() {})
+				.getBody();
+	}
 
-  @Override
-  public List<ShuttleResponse> getActiveShuttles() {
-    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + activeShuttlesURL);
+	@Override
+	public List<ShuttleResponse> getActiveShuttles() {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + activeShuttlesURL);
 
-    return restTemplate
-        .exchange(
-            builder.build().toUriString(),
-            HttpMethod.GET,
-            new HttpEntity<>(null, null),
-            new ParameterizedTypeReference<List<ShuttleResponse>>() {})
-        .getBody();
-  }
+		return restTemplate
+				.exchange(
+						builder.build().toUriString(),
+						HttpMethod.GET,
+						new HttpEntity<>(null, null),
+						new ParameterizedTypeReference<List<ShuttleResponse>>() {})
+				.getBody();
+	}
 
-  @Override
-  public CoordinateResponse getCoordinates(Integer vehicleID) {
+	@Override
+	public CoordinateResponse getCoordinates(Integer vehicleID) {
 
-    Map<String, Integer> params = new HashMap<>();
-    params.put("vehicleID", vehicleID);
+		Map<String, Integer> params = new HashMap<>();
+		params.put("vehicleID", vehicleID);
 
-    UriComponentsBuilder builder =
-        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForGet);
+		UriComponentsBuilder builder =
+				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForGet);
+		return restTemplate
+				.exchange(
+						builder.buildAndExpand(params).toUriString(),
+						HttpMethod.GET,
+						new HttpEntity<>(null, null),
+						new ParameterizedTypeReference<CoordinateResponse>() {})
+				.getBody();
+	}
 
-    return restTemplate
-        .exchange(
-            builder.buildAndExpand(params).toUriString(),
-            HttpMethod.GET,
-            new HttpEntity<>(null, null),
-            new ParameterizedTypeReference<CoordinateResponse>() {})
-        .getBody();
-  }
+	@Override
+	public VehicleOptionsResponse getVehicleOptions() {
+		UriComponentsBuilder builder =
+				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForVehicleOptions);
 
-  @Override
-  public VehicleOptionsResponse getVehicleOptions() {
-    UriComponentsBuilder builder =
-        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForVehicleOptions);
+		return restTemplate
+				.exchange(
+						builder.build().toUriString(),
+						HttpMethod.GET,
+						null,
+						new ParameterizedTypeReference<VehicleOptionsResponse>() {})
+				.getBody();
+	}
 
-    return restTemplate
-        .exchange(
-            builder.build().toUriString(),
-            HttpMethod.GET,
-            null,
-            new ParameterizedTypeReference<VehicleOptionsResponse>() {})
-        .getBody();
-  }
+	@Override
+	public ShuttleResponse markActive(Integer id) {
 
-  @Override
-  public ShuttleResponse markActive(Integer id) {
+		Map<String, Integer> params = new HashMap<>();
+		params.put("id", id);
 
-    Map<String, Integer> params = new HashMap<>();
-    params.put("id", id);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + markActiveURL);
 
-    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + markActiveURL);
+		return restTemplate
+				.exchange(
+						builder.buildAndExpand(params).toUriString(),
+						HttpMethod.PATCH,
+						new HttpEntity<>(null, null),
+						new ParameterizedTypeReference<ShuttleResponse>() {})
+				.getBody();
+	}
 
-    return restTemplate
-        .exchange(
-            builder.buildAndExpand(params).toUriString(),
-            HttpMethod.PATCH,
-            new HttpEntity<>(null, null),
-            new ParameterizedTypeReference<ShuttleResponse>() {})
-        .getBody();
-  }
+	@Override
+	public ShuttleResponse markInactive(Integer id) {
 
-  @Override
-  public ShuttleResponse markInactive(Integer id) {
+		Map<String, Integer> params = new HashMap<>();
+		params.put("id", id);
 
-    Map<String, Integer> params = new HashMap<>();
-    params.put("id", id);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + markInactiveURL);
 
-    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl + markInactiveURL);
+		return restTemplate
+				.exchange(
+						builder.buildAndExpand(params).toUriString(),
+						HttpMethod.PATCH,
+						new HttpEntity<>(null, null),
+						new ParameterizedTypeReference<ShuttleResponse>() {})
+				.getBody();
+	}
 
-    return restTemplate
-        .exchange(
-            builder.buildAndExpand(params).toUriString(),
-            HttpMethod.PATCH,
-            new HttpEntity<>(null, null),
-            new ParameterizedTypeReference<ShuttleResponse>() {})
-        .getBody();
-  }
+	@Override
+	public StartResponse startShift(StartRequest startRequest) {
 
-  @Override
-  public StartResponse startShift(StartRequest startRequest) {
+		UriComponentsBuilder builder =
+				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceStartOfShift);
 
-    UriComponentsBuilder builder =
-        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceStartOfShift);
+		return restTemplate
+				.exchange(
+						builder.build().toUriString(),
+						HttpMethod.POST,
+						new HttpEntity<>(startRequest),
+						new ParameterizedTypeReference<StartResponse>() {})
+				.getBody();
+	}
 
-    return restTemplate
-        .exchange(
-            builder.build().toUriString(),
-            HttpMethod.POST,
-            new HttpEntity<>(startRequest),
-            new ParameterizedTypeReference<StartResponse>() {})
-        .getBody();
-  }
+	@Override
+	public FuelResponse storeFuel(FuelRequest fuelRequest) {
+		UriComponentsBuilder builder =
+				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForFuel);
 
-  @Override
-  public FuelResponse storeFuel(FuelRequest fuelRequest) {
-    UriComponentsBuilder builder =
-        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForFuel);
+		return restTemplate
+				.exchange(
+						builder.build().toUriString(),
+						HttpMethod.POST,
+						new HttpEntity<>(fuelRequest),
+						new ParameterizedTypeReference<FuelResponse>() {})
+				.getBody();
+	}
 
-    return restTemplate
-        .exchange(
-            builder.build().toUriString(),
-            HttpMethod.POST,
-            new HttpEntity<>(fuelRequest),
-            new ParameterizedTypeReference<FuelResponse>() {})
-        .getBody();
-  }
+	@Override
+	public PassengerResponse storePassengers(PassengerRequest passengerRequest) {
 
-  @Override
-  public PassengerResponse storePassengers(PassengerRequest passengerRequest) {
+		UriComponentsBuilder builder =
+				UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForPassenger);
 
-    UriComponentsBuilder builder =
-        UriComponentsBuilder.fromUriString(baseUrl + shuttleServiceForPassenger);
-
-    return restTemplate
-        .exchange(
-            builder.build().toUriString(),
-            HttpMethod.POST,
-            new HttpEntity<>(passengerRequest),
-            new ParameterizedTypeReference<PassengerResponse>() {})
-        .getBody();
-  }
+		return restTemplate
+				.exchange(
+						builder.build().toUriString(),
+						HttpMethod.POST,
+						new HttpEntity<>(passengerRequest),
+						new ParameterizedTypeReference<PassengerResponse>() {})
+				.getBody();
+	}
 }
--- server/src/main/java/com/uline/shuttle/app/controllers/ShuttleAppController.java
+++ server/src/main/java/com/uline/shuttle/app/controllers/ShuttleAppController.java
@@ -1,9 +1,7 @@
 package com.uline.shuttle.app.controllers;
 
-import com.uline.common.metrics.ExecutionTime;
-import com.uline.shuttle.app.services.ShuttleAppService;
-import io.swagger.annotations.ApiOperation;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,6 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.uline.common.metrics.ExecutionTime;
+import com.uline.shuttle.app.services.ShuttleAppService;
+
+import io.swagger.annotations.ApiOperation;
 import rest.models.requests.CoordinateRequest;
 import rest.models.requests.FuelRequest;
 import rest.models.requests.PassengerRequest;
@@ -27,74 +30,74 @@ import rest.models.response.VehicleOptionsResponse;
 @RequestMapping("/api")
 public class ShuttleAppController {
 
-  private ShuttleAppService shuttleAppService;
+	private ShuttleAppService shuttleAppService;
 
-  @Autowired
-  public ShuttleAppController(ShuttleAppService shuttleAppService) {
-    this.shuttleAppService = shuttleAppService;
-  }
+	@Autowired
+	public ShuttleAppController(ShuttleAppService shuttleAppService) {
+		this.shuttleAppService = shuttleAppService;
+	}
 
-  @ExecutionTime("ShuttleAppService.enRoute")
-  @ApiOperation(
-      value = "posting the coordinates that we get from driver view and storing in a database")
-  @PatchMapping(value = "/enRoute")
-  public CoordinateResponse enRoute(@RequestBody CoordinateRequest coordinateRequest) {
-    return shuttleAppService.enRoute(coordinateRequest);
-  }
+	@ExecutionTime("ShuttleAppService.enRoute")
+	@ApiOperation(
+			value = "posting the coordinates that we get from driver view and storing in a database")
+	@PatchMapping(value = "/enRoute")
+	public CoordinateResponse enRoute(@RequestBody CoordinateRequest coordinateRequest) {
+		return shuttleAppService.enRoute(coordinateRequest);
+	}
 
-  @ExecutionTime("ShuttleAppService.getActiveShuttles")
-  @ApiOperation(value = "getting the active shuttles")
-  @PatchMapping(value = "/shuttles/active")
-  public List<ShuttleResponse> getActiveShuttles() {
-    return shuttleAppService.getActiveShuttles();
-  }
+	@ExecutionTime("ShuttleAppService.getActiveShuttles")
+	@ApiOperation(value = "getting the active shuttles")
+	@GetMapping(value = "/shuttles/active")
+	public List<ShuttleResponse> getActiveShuttles() {
+		return shuttleAppService.getActiveShuttles();
+	}
 
-  @ExecutionTime("ShuttleAppService.markActive")
-  @ApiOperation(value = "mark shuttle as active")
-  @PatchMapping(value = "/shuttles/{id}/active")
-  public ShuttleResponse markActive(@PathVariable("id") Integer id) {
-    return shuttleAppService.markActive(id);
-  }
+	@ExecutionTime("ShuttleAppService.markActive")
+	@ApiOperation(value = "mark shuttle as active")
+	@PatchMapping(value = "/shuttles/{id}/active")
+	public ShuttleResponse markActive(@PathVariable("id") Integer id) {
+		return shuttleAppService.markActive(id);
+	}
 
-  @ExecutionTime("ShuttleAppService.markInactive")
-  @ApiOperation(value = "mark shuttle as inactive")
-  @PatchMapping(value = "/shuttles/{id}/inactive")
-  public ShuttleResponse markInactive(@PathVariable("id") Integer id) {
-    return shuttleAppService.markInactive(id);
-  }
+	@ExecutionTime("ShuttleAppService.markInactive")
+	@ApiOperation(value = "mark shuttle as inactive")
+	@PatchMapping(value = "/shuttles/{id}/inactive")
+	public ShuttleResponse markInactive(@PathVariable("id") Integer id) {
+		return shuttleAppService.markInactive(id);
+	}
 
-  @ExecutionTime("ShuttleAppService.receiveCoordinates")
-  @ApiOperation("fetching coordinates from the database")
-  @GetMapping(value = "/receiveCoords/{vehicleID}")
-  public CoordinateResponse receiveCoordinates(@PathVariable("vehicleID") Integer vehicleID) {
-    return this.shuttleAppService.getCoordinates(vehicleID);
-  }
+	@ExecutionTime("ShuttleAppService.receiveCoordinates")
+	@ApiOperation("fetching coordinates from the database")
+	@GetMapping(value = "/receiveCoords/{vehicleID}")
+	public CoordinateResponse receiveCoordinates(@PathVariable("vehicleID") Integer vehicleID) {
+		return this.shuttleAppService.getCoordinates(vehicleID);
+	}
 
-  @ExecutionTime("ShuttleAppService.receiveVehicleOptions")
-  @ApiOperation(value = "fetching vehicles from database")
-  @GetMapping(value = "/receiveVehicleOptions")
-  public VehicleOptionsResponse receiveVehicleOptions() {
-    return shuttleAppService.getVehicleOptions();
-  }
+	@ExecutionTime("ShuttleAppService.receiveVehicleOptions")
+	@ApiOperation(value = "fetching vehicles from database")
+	@GetMapping(value = "/receiveVehicleOptions")
+	public VehicleOptionsResponse receiveVehicleOptions() {
+		return shuttleAppService.getVehicleOptions();
+	}
 
-  @ExecutionTime("ShuttleAppService.startShift")
-  @ApiOperation(value = "posting the start of shift details to the database")
-  @PostMapping(value = "/storeStartInformation")
-  public StartResponse startShift(@RequestBody StartRequest startRequest) {
-    return shuttleAppService.startShift(startRequest);
-  }
+	@ExecutionTime("ShuttleAppService.startShift")
+	@ApiOperation(value = "posting the start of shift details to the database")
+	@PostMapping(value = "/storeStartInformation")
+	public StartResponse startShift(@RequestBody StartRequest startRequest) {
+		return shuttleAppService.startShift(startRequest);
+	}
 
-  @ExecutionTime("ShuttleAppService.storeFuel")
-  @ApiOperation(value = "posting the fuel details to the database")
-  @PostMapping(value = "/storeFuel")
-  public FuelResponse storeFuel(@RequestBody FuelRequest fuelRequest) {
-    return shuttleAppService.storeFuel(fuelRequest);
-  }
+	@ExecutionTime("ShuttleAppService.storeFuel")
+	@ApiOperation(value = "posting the fuel details to the database")
+	@PostMapping(value = "/storeFuel")
+	public FuelResponse storeFuel(@RequestBody FuelRequest fuelRequest) {
+		return shuttleAppService.storeFuel(fuelRequest);
+	}
 
-  @ExecutionTime("ShuttleAppService.recordPassenger")
-  @ApiOperation(value = "posting the passenger amount details to the database")
-  @PostMapping(value = "/storePassengers")
-  public PassengerResponse storePassengers(@RequestBody PassengerRequest passengerRequest) {
-    return shuttleAppService.storePassengers(passengerRequest);
-  }
+	@ExecutionTime("ShuttleAppService.recordPassenger")
+	@ApiOperation(value = "posting the passenger amount details to the database")
+	@PostMapping(value = "/storePassengers")
+	public PassengerResponse storePassengers(@RequestBody PassengerRequest passengerRequest) {
+		return shuttleAppService.storePassengers(passengerRequest);
+	}
 }
