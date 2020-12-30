87d088796f1194278dc05a48c62259071fe17ec6
Diontre.Pate <diontre.pate@uline.com>
2020-12-29 20:30:44 -0600

    adding unit tests

---
+++ server/src/test/java/com/uline/shuttle/configuration/AppConfigTest.java
@@ -0,0 +1,24 @@
+package com.uline.shuttle.configuration;
+
+import com.uline.ha.rest.UlineRestTemplate;
+import com.uline.security.service.UserResolver;
+import com.uline.shuttle.app.configuration.AppConfig;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+public class AppConfigTest {
+
+    @Mock
+    UlineRestTemplate restTemplate;
+    @Mock
+    UserResolver userResolver;
+
+
+    @Test
+    public void testTokenInterceptor() {
+        AppConfig appConfig = new AppConfig();
+        Assert.assertNotNull(appConfig.tokenInterceptor(new UlineRestTemplate(new HttpComponentsClientHttpRequestFactory()), userResolver));
+    }
+}
